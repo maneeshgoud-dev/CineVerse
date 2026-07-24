412985c29ab1565e1a1df3c76de44d58ec389de7
Maneesh <maneeshgoud.dev@gmail.com>
2026-07-24 19:22:40 +0530

    inital commit

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React from "react";
 
-const MovieCard = ({ movie:
-  { title, vote_average, poster_path, release_date, original_language }
+const MovieCard = ({
+  movie: {
+    title,
+    vote_average,
+    poster_path,
+    release_date,
+    original_language,
+    poster_url,
+  },
 }) => {
   return (
     <div className="movie-card">
       <img
-        src={poster_path ?
-          `https://image.tmdb.org/t/p/w500/${poster_path}` : '/no-movie.png'}
+        src={
+          poster_url ||
+          (poster_path
+            ? `https://image.tmdb.org/t/p/w500/${poster_path}`
+            : "/no-movie.png")
+        }
         alt={title}
       />
 
@@ -17,7 +28,7 @@ const MovieCard = ({ movie:
         <div className="content">
           <div className="rating">
             <img src="star.svg" alt="Star Icon" />
-            <p>{vote_average ? vote_average.toFixed(1) : 'N/A'}</p>
+            <p>{vote_average ? vote_average.toFixed(1) : "N/A"}</p>
           </div>
 
           <span>•</span>
@@ -25,11 +36,11 @@ const MovieCard = ({ movie:
 
           <span>•</span>
           <p className="year">
-            {release_date ? release_date.split('-')[0] : 'N/A'}
+            {release_date ? release_date.split("-")[0] : "N/A"}
           </p>
         </div>
       </div>
     </div>
-  )
-}
-export default MovieCard
+  );
+};
+export default MovieCard;
---
+++ src/fallbackMovies.js
@@ -0,0 +1,49 @@
+const fallbackMovies = [
+  {
+    id: 1,
+    title: "Interstellar",
+    vote_average: 8.7,
+    poster_path: null,
+    release_date: "2014-11-07",
+    original_language: "en",
+    poster_url: "/no-movie.png",
+  },
+  {
+    id: 2,
+    title: "The Matrix",
+    vote_average: 8.2,
+    poster_path: null,
+    release_date: "1999-03-31",
+    original_language: "en",
+    poster_url: "/no-movie.png",
+  },
+  {
+    id: 3,
+    title: "Inception",
+    vote_average: 8.4,
+    poster_path: null,
+    release_date: "2010-07-16",
+    original_language: "en",
+    poster_url: "/no-movie.png",
+  },
+  {
+    id: 4,
+    title: "Dune",
+    vote_average: 7.9,
+    poster_path: null,
+    release_date: "2021-09-15",
+    original_language: "en",
+    poster_url: "/no-movie.png",
+  },
+  {
+    id: 5,
+    title: "Arrival",
+    vote_average: 7.9,
+    poster_path: null,
+    release_date: "2016-11-11",
+    original_language: "en",
+    poster_url: "/no-movie.png",
+  },
+];
+
+export default fallbackMovies;
